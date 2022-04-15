791914ccc2660ae769e3871ad80926d3b47e7242
Jacob Arthurs <jacob.arthurs@bamtech.net>
2022-04-15 11:52:18 -0500

    Updated "Program.cs" to have explicitly declared operationId to help with openAPI naming errors

--- bike-stores-api/Program.cs
+++ bike-stores-api/Program.cs
@@ -11,7 +11,11 @@ builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    //c.CustomOperationIds(e => $"{e.HttpMethod?.ToLower()}{e.ActionDescriptor.RouteValues["controller"]}{(e.RelativePath!.Contains("id")? "Id" : "" )}");
+    c.CustomOperationIds(e => $"{e.HttpMethod?.ToLower()}{char.ToUpper(e.RelativePath![0]) + e.RelativePath.Substring(1)}");
+});
 
 var app = builder.Build();
 
